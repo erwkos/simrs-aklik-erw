f25f2e82e35474398b7f46413b2184048fa4864e
erwkos <erwkos84@gmail.com>
2024-10-10 15:37:41 +0700

    Perubahan metafisik admin

--- ilogic/metafisik/admin.py
+++ ilogic/metafisik/admin.py
@@ -7,9 +7,12 @@ from metafisik.models import NoBAMetafisik, DataKlaimCBGMetafisik
 # Register your models here.
 @admin.register(NoBAMetafisik)
 class NoBAMetafisikAdmin(ImportExportModelAdmin):
-    search_fields = ['no_surat_bast', 'kdkrlayan', 'kdkclayan', 'kdppklayan', 'nmppklayan']
+    search_fields = ['no_surat_bast', 'kdkrlayan', 'kdkclayan', 'kdppklayan', 'nmppklayan', 'tgl_bast']
 
 
 @admin.register(DataKlaimCBGMetafisik)
 class DataKlaimCBGMetafisikAdmin(ImportExportModelAdmin):
     search_fields = ['no_bast__no_surat_bast', 'nosjp', 'no_bast__tgl_bast']
+    list_filter = [
+        ('no_bast__tgl_bast', admin.DateFieldListFilter),
+    ]
